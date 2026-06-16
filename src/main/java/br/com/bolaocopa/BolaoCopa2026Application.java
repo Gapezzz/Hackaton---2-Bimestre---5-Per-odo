e3271124cb4ff6afc5664396fadd78ff4aff543f
Pestanadev <pestanadev@gmail.com>
2026-06-16 20:47:20 -0300

    commit inicial

---
+++ src/main/java/br/com/bolaocopa/BolaoCopa2026Application.java
@@ -0,0 +1,13 @@
+package br.com.bolaocopa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolaoCopa2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolaoCopa2026Application.class, args);
+	}
+
+}
